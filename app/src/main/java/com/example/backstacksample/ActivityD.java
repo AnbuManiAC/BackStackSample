6da9431cb82a06654258622597a3ffefad8c70b3
Anbumani AC <anbu-pt4564@anbu-pt4564.csez.zohocorpin.com>
2022-05-11 10:55:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/backstacksample/ActivityD.java
@@ -0,0 +1,62 @@
+package com.example.backstacksample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ActivityD extends BaseActivity {
+    private static final String TAG=ActivityD.class.getSimpleName();
+    private static int instanceCounter=0;
+    private int currentInstanceValue;
+
+    private Button buttonStartActivityA,buttonStartActivityB,buttonStartActivityC,buttonStartActivityD;
+    private TextView textViewTaskInfo,textViewInstanceValue;
+
+    public ActivityD(){
+        super();
+        instanceCounter++;
+        currentInstanceValue=instanceCounter;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d);
+
+        buttonStartActivityA=(Button)findViewById(R.id.buttonStartActivityA);
+        buttonStartActivityB=(Button)findViewById(R.id.buttonStartActivityB);
+        buttonStartActivityC=(Button)findViewById(R.id.buttonStartActivityC);
+        buttonStartActivityD=(Button)findViewById(R.id.buttonStartActivityD);
+
+        textViewTaskInfo=(TextView)findViewById(R.id.textViewTaskInfo);
+        textViewInstanceValue=(TextView)findViewById(R.id.textViewInstanceValue);
+        textViewInstanceValue.append(",Current instance: "+currentInstanceValue);
+
+        buttonStartActivityA.setOnClickListener(this);
+        buttonStartActivityB.setOnClickListener(this);
+        buttonStartActivityC.setOnClickListener(this);
+        buttonStartActivityD.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonStartActivityA: startActivity(this,ActivityA.class);break;
+            case R.id.buttonStartActivityB: startActivity(this,ActivityB.class);break;
+            case R.id.buttonStartActivityC: startActivity(this,ActivityC.class);break;
+            case R.id.buttonStartActivityD: startActivity(this,ActivityD.class);break;
+            default:break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"Instances: "+currentInstanceValue);
+        textViewTaskInfo.setText(getAppTaskState());
+    }
+}
